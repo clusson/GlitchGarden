2b741967f980a29017886583592662ed1dbbcd3a
clusson <clementlusson@gmail.com>
2017-08-31 17:21:33 +0200

    set First/Start scenes with bk images

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start (){
+		Invoke("LoadNextLevel",autoLoadNextLevelAfter);
+	}
+
+	public void LoadLevel (string name)
+	{
+		Debug.Log("Level load requested for: "+name);
+		SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest()
+	{
+		Debug.Log("I want to quit");
+		Application.Quit();
+	}
+
+	public void LoadNextLevel() {
+		SceneManager.LoadScene(Application.loadedLevel + 1);
+	}
+
+}
