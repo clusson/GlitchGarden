34c81ce792f475149fa464a73603bd7075e29387
clusson <clementlusson@gmail.com>
2017-09-01 10:54:30 +0200

    add menu + options

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,8 +7,13 @@ public class LevelManager : MonoBehaviour {
 
 	public float autoLoadNextLevelAfter;
 
-	void Start (){
-		Invoke("LoadNextLevel",autoLoadNextLevelAfter);
+	void Start ()
+	{
+		if (autoLoadNextLevelAfter == 0) {
+			Debug.Log("Level auto load disabled"); 
+		} else {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
 	}
 
 	public void LoadLevel (string name)
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void OnLevelWasLoaded (int level)
+	{
+		AudioClip thisLevelMusic = levelMusicChangeArray [level];
+		Debug.Log ("Playing clip: " + thisLevelMusic);
+
+		if (thisLevelMusic) {
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
