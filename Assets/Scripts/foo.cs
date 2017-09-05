3b199d56b1cead7646d12b0a50a482ad59b327bd
clusson <clementlusson@gmail.com>
2017-09-05 10:36:08 +0200

    7 - 157 Done

---
+++ Assets/Scripts/foo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void TestMethod () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
