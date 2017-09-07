19c522c4fe197ff781fa1c4707ec56a643535aa0
clusson <clementlusson@gmail.com>
2017-09-07 13:24:54 +0200

    game is finished

--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class Stone : MonoBehaviour {
 
-	// Use as a tage for now 
+	private Animator animator;
+	
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		
+	}
+	
+	void OnTriggerStay2D (Collider2D collider) {
+		Attacker attacker = collider.gameObject.GetComponent<Attacker>();
+		
+		if (attacker) {
+			animator.SetTrigger ("underAttack trigger");
+		}
+	}
 }
